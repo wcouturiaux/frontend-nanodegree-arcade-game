add94691d081205a68a274060410343911ae55f9
Will C <w.l.couturiaux@gmail.com>
2018-05-23 00:22:25 -0400

    Split enemy position into separate x and y properties to match render method

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ var Enemy = function(x,y,speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.position = [x,y];
+    this.x = x;
+    this.y = y;
     this.speed = speed;
 };
 
@@ -16,7 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.position[0] += this.speed * dt;
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -34,7 +35,7 @@ var Player = function(x,y) {
 
 Player.prototype.update = function(dt) {
 
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
